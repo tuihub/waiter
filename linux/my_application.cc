f798d4d18e1e30e5137a682633f96f2130eb7eb9
MuZhou233 <muzhou233@outlook.com>
2023-10-08 15:27:06 +0100

    chore: change app name

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "waiter");
+    gtk_header_bar_set_title(header_bar, "TuiHub");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "waiter");
+    gtk_window_set_title(window, "TuiHub");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
